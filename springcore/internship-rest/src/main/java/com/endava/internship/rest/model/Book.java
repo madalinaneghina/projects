74771ae884fde5e7e553a1410cdb5025c5f513cd
Madalina Neghina <Madalina.Neghina@endava.com>
2016-07-15 18:08:18 +0300

    spring mvc rest

---
+++ springcore/internship-rest/src/main/java/com/endava/internship/rest/model/Book.java
@@ -0,0 +1,21 @@
+package com.endava.internship.rest.model;
+
+public class Book {
+	private String name;
+
+	public Book(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
